00a140b84a7eb7a86deeb88630eca28a01858fd4
Jorit Vásconez Gerlach <jorit@vasconezgerlach.de>
2021-09-05 23:33:20 +0200

    Create JS

---
+++ page.js
@@ -0,0 +1,17 @@
+function topIsBigger() {
+    const topBox = document.getElementById('top');
+    const botBox = document.getElementById('bot');
+    
+    topBox.style.height = '70%';
+    botBox.style.height = '30%';
+}
+function botIsBigger() {
+    const topBox = document.getElementById('top');
+    const botBox = document.getElementById('bot');
+    
+    
+    botBox.style.height = '70%';
+    botBox.style.borderRadius = '200px 200px 0 0';
+    botBox.style.borderRadius = '0';
+    topBox.style.height = '30%';
+}
