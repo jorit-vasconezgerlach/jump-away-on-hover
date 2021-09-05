00b0fe2dfa8a9f5e0bf666cc181ca811997d8439
Jorit Vásconez Gerlach <jorit@vasconezgerlach.de>
2021-09-05 23:40:26 +0200

    Update JS + restructure

--- page.js
+++ page.js
@@ -1,15 +1,14 @@
 function topIsBigger() {
     const topBox = document.getElementById('top');
     const botBox = document.getElementById('bot');
-    
+
     topBox.style.height = '70%';
     botBox.style.height = '30%';
 }
 function botIsBigger() {
     const topBox = document.getElementById('top');
     const botBox = document.getElementById('bot');
-    
-    
+
     botBox.style.height = '70%';
     botBox.style.borderRadius = '200px 200px 0 0';
     botBox.style.borderRadius = '0';
